5f9d74ed6c12a283f8bf10d3559d8356089a0f88
cheesheahan794 <cheesheahan794@gmail.com>
2025-04-05 04:49:07 +0000

    Add 24.ts

---
+++ src/24.ts
@@ -0,0 +1,8 @@
+// Define your own functions and expressions here
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
